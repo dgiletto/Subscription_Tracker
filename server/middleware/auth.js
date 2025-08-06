8c9c3bc0b19209c5e7ee28aa6452249e62c4c340
dgiletto <dgiletto@udel.edu>
2025-08-06 14:06:54 -0400

    Using .env for Firebase keys instead of entire json file

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,11 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../trakr-efefe-firebase-adminsdk-fbsvc-2a5c75687e.json' with { type: 'json' };
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
 });
 
 export const verifyToken = async (req, res, next) => {
